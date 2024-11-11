399c5baf6fd1f8eb8823d1e6f4a0b03d8260ba15
ushaaaaaaaa <ushasripalukuri1410@gmail.com>
2024-11-11 13:46:51 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -2,7 +2,7 @@ class Solution:
    
     def getTotalIsles(self, grid: list[list[str]]) -> int:
     #    write your code here
-                    
+                     if not grid or not grid[0]:
         return 0
 class Solution:
     def getTotalIsles(self, grid: list[list[str]]) -> int:
